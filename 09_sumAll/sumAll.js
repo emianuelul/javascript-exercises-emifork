0b3340eb192c4dd3acabbd1d1a5f45597e9de347
emianuelul <emanuel.marin0000@gmail.com>
2025-05-29 20:16:33 +0300

    solve: 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // CHECKS
+    if( (a < 0 || b < 0 ) ||
+        (!Number.isInteger(a) || !Number.isInteger(b))){
+        return "ERROR";
+    }
+    
 
+    let sum = 0;
+    if(a < b)
+        for(let i = a; i <= b; ++i)
+        {
+            sum += i;
+        }
+    
+    else if(a > b)
+        for(let i = a; i >= b; --i)
+        {
+            sum += i;
+        }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
