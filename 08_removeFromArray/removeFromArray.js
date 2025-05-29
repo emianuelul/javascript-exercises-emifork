b2713cfb6a581018ff81054c4028d8c59238f5d5
emianuelul <emanuel.marin0000@gmail.com>
2025-05-29 20:10:05 +0300

    solve: 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+
+    let solution = [];
+
+    solution = arr.filter((item) => {return !theArgs.includes(item)});
+    
+    return solution;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
