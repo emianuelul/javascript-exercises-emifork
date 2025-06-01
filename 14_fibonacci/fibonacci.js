b99b2dbe32a234c50e1d0aa17b1fc422e120cacf
emianuelul <emanuel.marin0000@gmail.com>
2025-06-01 18:03:04 +0300

    solved: 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS";
+    }
+    
+    let number = parseInt(a);
 
+    if(number === 0){
+        return 0;
+    }
+    else if(number === 1){
+        return 1;
+    }
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
 // Do not edit below this line
