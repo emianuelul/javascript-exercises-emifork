9d16b06ee69f27bb20123e7f24fcbaaeefc6c1ae
emianuelul <emanuel.marin0000@gmail.com>
2025-06-01 17:39:14 +0300

    solved: 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let reverse = str.toLowerCase()
+                     .split('')
+                     .reverse();
 
+    reverse = reverse.filter((item) => {
+        return ((item >= 'a' && item <= 'z') || (item >= '0' && item <= '9'));
+    });
+    reverse = reverse.join('');
+
+    let cleanedStr = str.toLowerCase()
+                        .split('');
+    cleanedStr = cleanedStr.filter((item) => {
+        return ((item >= 'a' && item <= 'z') || (item >= '0' && item <= '9'));
+    });
+    cleanedStr = cleanedStr.join('');
+
+    return reverse === cleanedStr;
 };
 
 // Do not edit below this line
