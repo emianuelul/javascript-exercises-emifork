751505b9dc69027394c8ef9f4858b3cb72731743
emianuelul <emanuel.marin0000@gmail.com>
2025-06-03 21:14:12 +0300

    solve: 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+    let max = -1;
+    let oldest;
+    const currentDate = new Date();
+    const year = currentDate.getFullYear(); 
 
+    for(const person of arr){
+        if(person.yearOfDeath != undefined){
+            if (max < person.yearOfDeath - person.yearOfBirth){
+                oldest = person;
+                max = person.yearOfDeath - person.yearOfBirth;
+            }
+        }
+        else{
+            if(max < year - person.yearOfBirth){
+                oldest = person;
+                max = year - person.yearOfBirth;
+            }
+        }
+    }
+
+    return oldest;
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
